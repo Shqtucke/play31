753daccc5524f50ee041ebb32956dd276bd113a8
Shaun Tucker <tucke@shauns-air.home>
2018-03-30 18:25:39 -0400

    Assets added

---
+++ play31/Controller/CustomTab.swift
@@ -0,0 +1,23 @@
+//
+//  CustomTab.swift
+//  play31
+//
+//  Created by Shaun Tucker on 3/30/18.
+//  Copyright © 2018 Shaun Tucker. All rights reserved.
+//
+
+import UIKit
+
+class CustomTab: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .green 
+    }
+
+
+    
+
+
+}
